8c85c074a9dbff90252172e51e5a1efb68614747
Nicklas H. <gta5.r6s.bf4@gmail.com>
2024-04-29 12:14:54 +0200

    Improved type detection in Settings

--- lib/settings.py
+++ lib/settings.py
@@ -1,10 +1,12 @@
-import misc
+from lib import misc, menu  # noqa
+import logging  # noqa
 
 
 class Settings:
-    try:
-        ffmpeg_is_installed = misc.ffmpeg_is_installed()
-    except Exception:
-        ffmpeg_is_installed = False
-    logger = None
-    download_type = None
+    def __init__(self):
+        try:
+            self.ffmpeg_is_installed = misc.ffmpeg_is_installed()
+        except Exception:
+            self.ffmpeg_is_installed = False
+        self.logger = None  # type: logging.Logger
+        self.download_type = None  # type: menu.DownloadType
