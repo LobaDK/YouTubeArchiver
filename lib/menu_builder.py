099d61ef8772641e77a518dea3f841c135cb6a52
LobaDK <lobadk@lobadk.com>
2024-05-14 00:53:00 +0200

    Introduced menu_builder module for menu overhauling

---
+++ lib/menu_builder.py
@@ -0,0 +1,12 @@
+"""
+TODO:
+1. Create a MenuBuilder class that can do the following:
+    - Build a list of InquirerPy choices, based on current settings. The possible choices will be provided by another class.
+    - Prompt the user with the choices, and return the user's selection.
+
+2. Create a Menu class that act as the actual menu, doing the following:
+    - Provides a list of choices for the MenuBuilder to use.
+    - Handles the user's selection.
+
+3. Some form of place to store the choices, how to handle each choice, and functions required for any choices, most likely a class for each menu.
+"""
